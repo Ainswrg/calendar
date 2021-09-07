2814057044b548af65f85cc89d438d6614bae044
Jaslan <Jaslan.dlnv@gmail.com>
2021-09-07 13:03:58 +0500

    setting-up state management

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { store } from './store';
 
-ReactDOM.render(
-    <App />,
+render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
