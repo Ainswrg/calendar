0de26d1c93a7730061506f5eb1de8b89bbf8657c
Jaslan <Jaslan.dlnv@gmail.com>
2021-09-07 19:28:50 +0500

    feat: added custom hook useTypedSelector

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 import { privateRoutes, publicRoutes, RouteNames } from '../routes';
 
 const AppRouter = () => {
-  const auth = false;
-  return auth ? (
+  const { isAuth } = useTypedSelector((state) => state.auth);
+  return isAuth ? (
     <Switch>
       {privateRoutes.map((route) => (
         <Route key={route.path} path={route.path} exact={route.exact} component={route.component} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,15 @@ import { FC } from 'react';
 import { Layout, Menu, Row } from 'antd';
 import { useHistory } from 'react-router-dom';
 import { RouteNames } from '../routes';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 export const Navbar: FC = () => {
   const router = useHistory();
-  const auth = false;
+  const { isAuth } = useTypedSelector((state) => state.auth);
   return (
     <Layout.Header>
       <Row justify="end">
-        {auth ? (
+        {isAuth ? (
           <>
             <div style={{ color: 'white' }}>Jaslan D</div>
             <Menu theme="dark" mode="horizontal" selectable={false}>
